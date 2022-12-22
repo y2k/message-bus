d86baf2dab02c4dbba16b63805cfb4dbcd3b5961
y2k <itwisterlx@gmail.com>
2022-12-22 15:20:35 +0400

    refactoring

--- clojurescript/src/main/lispmachine/worker.cljs
+++ clojurescript/src/main/lispmachine/worker.cljs
@@ -1,54 +1,10 @@
 (ns lispmachine.worker)
 
-; async fetch(request, env, ctx) {
-; 	return new Response("Hello from Y2k");
-; },
+(defn- fetch [request env]
+  (->
+   env.MESSAGE_LOG
+   (.prepare "SELECT topic, data FROM log ORDER BY _id LIMIT 10")
+   (.all)
+   (.then (fn [xs] (js/Response.json xs.results)))))
 
-;; isTrusted
-;; request
-;; \_ body,bodyUsed,headers,cf,signal,redirect,url,method
-
-
- ;; if (pathname === "/api/beverages")  {
-    ;;   const { results } = await env.<BINDING_NAME>.prepare(
-    ;;     "SELECT * FROM Customers WHERE CompanyName = ?"
-    ;;   )
-    ;;     .bind("Bs Beverages")
-    ;;     .all();
-    ;;   return Response.json(results)
-    ;; }
-
-#_(js/addEventListener
- "fetch"
- (fn [event]
-   (->
-    #_(.prepare js/MESSAGE_LOG "SELECT * FROM log")
-    #_(.all)
-    (js/Promise.resolve 0)
-    (.then (fn [x] (js/Response.json "hello")))
-    (.then (fn [x] (.respondWith event x)))
-    )
-   ))
-
-(def eventHandlers
- #js {:fetch
-      (fn [request env]
-        #_(js/Response. (str "ML:" (js/Object.keys env)))
-        (->
-         env.MESSAGE_LOG
-         (.prepare "SELECT * FROM log")
-         (.all)
-         (.then (fn [x] (js/Response.json x.results)))))})
-
-#_(def ^:export fetch
-  (fn [req]
-    (js/Response. "hello world")))
-
-#_(js/addEventListener
- "fetch"
-  (fn [event]
-    (.respondWith
-      event
-      #_(js/Response.json (.get js/MESSAGES "foo"))
-      (js/Response.json js/MESSAGE_LOG)
-      )))
+(def eventHandlers #js {:fetch fetch})
