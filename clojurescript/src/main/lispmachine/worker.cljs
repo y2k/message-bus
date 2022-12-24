86bfe5945d0ceddf326b4f439fa541e304afe129
y2k <itwisterlx@gmail.com>
2022-12-25 01:16:06 +0400

    add sqlite

--- clojurescript/src/main/lispmachine/worker.cljs
+++ clojurescript/src/main/lispmachine/worker.cljs
@@ -1,10 +1,35 @@
 (ns lispmachine.worker)
 
 (defn- fetch [request env]
-  (->
-   env.MESSAGE_LOG
-   (.prepare "SELECT topic, data FROM log ORDER BY _id LIMIT 10")
-   (.all)
-   (.then (fn [xs] (js/Response.json xs.results)))))
+  (let [path (.-pathname (js/URL. request.url))
+        r1 (.exec #"/([a-z]+)/(\d+)" path)]
+    (if (and (= "GET" request.method) (some? r1))
+      (->
+       env.MESSAGE_LOG
+       (.prepare "SELECT id, topic, data FROM log WHERE topic = ?1 AND id >= ?2 ORDER BY id LIMIT 1")
+       (.bind (.at r1 1) (.at r1 2))
+       (.first)
+       (.then
+        (fn [xs]
+          (if (some? xs)
+            (js/Response.
+             (js/atob xs.data)
+             (clj->js {:headers
+                       {:x-id xs.id
+                        :content-type "application/octet-stream"}}))
+            (js/Response. "" #js{:status 204})))))
+      (let [r1 (.exec #"/([a-z]+)" path)]
+        (if (and (= "POST" request.method) (some? r1))
+          (->
+           (.text request)
+           (.then
+            (fn [ab]
+              (->
+               env.MESSAGE_LOG
+               (.prepare "INSERT INTO log (topic, data) VALUES (?1, ?2)")
+               (.bind (.at r1 1) (js/btoa ab))
+               (.run)
+               (.then
+                (fn [r] (js/Response. "" #js{:status 204}))))))))))))
 
 (def eventHandlers #js {:fetch fetch})
