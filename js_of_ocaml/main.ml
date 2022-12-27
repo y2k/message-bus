3b090de1bbc0e523bb3e308cbaa1b29cfd789c81
y2k <itwisterlx@gmail.com>
2022-12-27 23:19:22 +0300

    add jsoo

--- js_of_ocaml/main.ml
+++ js_of_ocaml/main.ml
@@ -1,2 +1,10 @@
+open Js_of_ocaml
+module U = Js.Unsafe
+
+let response_with event body =
+  event##respondWith (U.new_obj (U.pure_js_expr "Response") [| U.inject body |])
+  |> ignore
+
 let () =
-  print_endline "hello from ocaml"
+  U.global##addEventListener "fetch3" (fun event ->
+      response_with event "event")
