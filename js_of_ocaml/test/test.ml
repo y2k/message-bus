e30e760fa92d275ceefe740741adeedac0fbd04b
y2k <itwisterlx@gmail.com>
2023-01-05 13:17:23 +0400

    fix tests

--- js_of_ocaml/test/test.ml
+++ js_of_ocaml/test/test.ml
@@ -32,18 +32,6 @@ let make_tests ?headers rules =
             (Core.handle (make_msg input)) ) )
     rules
 
-module TestSample = struct
-  type t = {expected: string option; input: string; headers: string StringMap.t}
-
-  let make expected input ?headers =
-    { expected
-    ; input
-    ; headers=
-        Option.value headers
-          ~default:
-            (StringMap.singleton "x-telegram-bot-api-secret-token" "token") }
-end
-
 let samples =
   [ (None, {|{}|})
   ; ( None
@@ -54,7 +42,7 @@ let samples =
     ) ]
   |> make_tests
 
-let auth =
+let auth_base =
   [ (None, {|{}|})
   ; ( None
     , {|{"update_id":569999999,"message":{"message_id":4699,"from":{"id":249999999,"is_bot":false,"first_name":"JohnDoe","username":"johndoe","language_code":"en"},"chat":{"id":249999999,"first_name":"JohnDoe","username":"johndoe","type":"private"},"date":1699999999,"text":"hello"}}|}
@@ -65,6 +53,13 @@ let auth =
   ; ( None
     , {|{"update_id":560000000,"message":{"message_id":12000,"from":{"id":240000000,"is_bot":false,"first_name":"Alex","username":"alex000","language_code":"en"},"chat":{"id":-1001000000000,"title":"GroupName","type":"supergroup"},"date":1600000000,"new_chat_participant":{"id":1300000000,"is_bot":true,"first_name":"Docker","username":"docker_bot"},"new_chat_member":{"id":1300000000,"is_bot":true,"first_name":"Docker","username":"docker_bot"},"new_chat_members":[{"id":1300000000,"is_bot":true,"first_name":"Docker","username":"docker_bot"}]}}|}
     ) ]
-  |> make_tests ~headers:M.empty
 
-let () = run "tests" [("message parse", samples); ("auth", auth)]
+let auth = auth_base |> make_tests ~headers:M.empty
+
+let auth2 =
+  auth_base
+  |> make_tests
+       ~headers:(M.singleton "x-telegram-bot-api-secret-token" "token2")
+
+let () =
+  run "tests" [("message parse", samples); ("auth", auth); ("auth2", auth2)]
